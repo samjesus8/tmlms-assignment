c2e6e041e9a922ab04e39e0a434a35ea118d9a41
Samuel Jesuthas <samueljesuthas27@gmail.com>
2022-10-03 23:52:03 +0100

    Laid out Fundamentals
    -Changed buttons for a "Login" & "Exit" button
    -Added 3 forms for Admin/Instructor/Student pages
    -Resized main window and removed any uneccesary code

--- TmLms/Form1.Designer.cs
+++ TmLms/Form1.Designer.cs
@@ -29,8 +29,9 @@
         private void InitializeComponent()
         {
             this.comboBoxUserType = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // comboBoxUserType
@@ -38,53 +39,65 @@
             this.comboBoxUserType.FormattingEnabled = true;
             this.comboBoxUserType.Items.AddRange(new object[] {
             "Admin",
-            "Advance User",
-            "Simple User"});
-            this.comboBoxUserType.Location = new System.Drawing.Point(26, 26);
+            "Instructor",
+            "Student"});
+            this.comboBoxUserType.Location = new System.Drawing.Point(26, 60);
             this.comboBoxUserType.Margin = new System.Windows.Forms.Padding(2);
             this.comboBoxUserType.Name = "comboBoxUserType";
             this.comboBoxUserType.Size = new System.Drawing.Size(129, 23);
             this.comboBoxUserType.TabIndex = 0;
             // 
-            // button1
+            // loginButton
             // 
-            this.button1.Location = new System.Drawing.Point(32, 104);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 37);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Create Course";
-            this.button1.UseVisualStyleBackColor = true;
+            this.loginButton.Location = new System.Drawing.Point(26, 119);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(129, 55);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
             // 
-            // button2
+            // exitButton
             // 
-            this.button2.Location = new System.Drawing.Point(171, 104);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 37);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Create Module";
-            this.button2.UseVisualStyleBackColor = true;
+            this.exitButton.Location = new System.Drawing.Point(215, 119);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(129, 55);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(341, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Student Management System | V1.0 Alpha Beta Gamma Omega";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(560, 270);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(356, 195);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.loginButton);
             this.Controls.Add(this.comboBoxUserType);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private ComboBox comboBoxUserType;
-        private Button button1;
-        private Button button2;
+        private Button loginButton;
+        private Button exitButton;
+        private Label label1;
     }
 }
--- TmLms/Form1.cs
+++ TmLms/Form1.cs
@@ -1,3 +1,5 @@
+using TmLms.UserForms;
+
 namespace TmLms
 {
     public partial class Form1 : Form
@@ -5,6 +7,30 @@ namespace TmLms
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if (comboBoxUserType.SelectedIndex == 0) //Admin Open
+            {
+                AdminForm adminForm = new AdminForm();
+                adminForm.Show();
+            }
+            if (comboBoxUserType.SelectedIndex == 1) //Instructor (Teacher) Open
+            {             
+                InstructorForm instructorForm = new InstructorForm();
+                instructorForm.Show();
+            }
+            if (comboBoxUserType.SelectedIndex == 2) //Student Open
+            {               
+                StudentForm studentForm = new StudentForm();
+                studentForm.Show();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
